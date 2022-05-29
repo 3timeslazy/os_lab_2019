c47bb28b8b0c16b90aaad982094a8192f537eecb
Vladimir Fetisov <vyufetisov@gmail.com>
2022-05-29 22:09:40 +0000

    add parseOpts

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include <getopt.h>
 
 #include <pthread.h>
+
+#include "parallel_sum.h"
 
 struct SumArgs {
   int *array;
@@ -22,12 +27,8 @@ void *ThreadSum(void *args) {
 }
 
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
+  struct CmdOpts cmd_opts = parseOpts(argc, argv);
+  // printf("threads_num: %d, array_msize: %d, seed: %d\n", cmd_opts.threads_num, cmd_opts.array_size, cmd_opts.seed);
 
   uint32_t threads_num = 0;
   uint32_t array_size = 0;
@@ -60,4 +61,71 @@ int main(int argc, char **argv) {
   free(array);
   printf("Total: %d\n", total_sum);
   return 0;
+}
+
+struct CmdOpts parseOpts(int argc, char **argv) {
+  struct CmdOpts cmd_opts = {-1, -1, -1};
+
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"seed", required_argument, 0, 0},
+                                      {"array_size", required_argument, 0, 0},
+                                      {"threads_num", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "f", options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 0:
+        switch (option_index) {
+          case 0:
+            cmd_opts.seed = atoi(optarg);
+            if (cmd_opts.seed <= 0) {
+              printf("seed must be a positive number\n");
+              exit(1);
+            }
+            break;
+          case 1:
+            cmd_opts.array_size = atoi(optarg);
+            if (cmd_opts.array_size <= 0) {
+              printf("array_size must be a positive number\n");
+              exit(1);
+            }
+            break;
+          case 2:
+            cmd_opts.threads_num = atoi(optarg);
+            if (cmd_opts.threads_num <= 0) {
+              printf("threads_num must be a positive number\n");
+              exit(1);
+            }
+            break;
+          default:
+            printf("Index %d is out of options\n", option_index);
+        }
+        break;
+
+      case '?':
+        break;
+
+      default:
+        printf("getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (optind < argc) {
+    printf("Has at least one no option argument\n");
+    exit(1);
+  }
+
+  if (cmd_opts.seed == -1 || cmd_opts.array_size == -1 || cmd_opts.threads_num == -1) {
+    printf("Usage: %s --seed \"num\" --array_size \"num\" --threads_num \"num\" \n",
+           argv[0]);
+    exit(1);
+  }
+
+  return cmd_opts;
 }
